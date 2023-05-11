7e07ffa61abc9154096198d476b45be80c8e3567
Novita Siti Julaeha <novitasitijulaeha03@gmail.com>
2023-05-11 19:37:41 +0700

    second commit

--- flutter_b_novita_siti_julaeha/project/wedplan/lib/views/checklist/check_list_screen.dart
+++ flutter_b_novita_siti_julaeha/project/wedplan/lib/views/checklist/check_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:wedplan/view_model/checklist_manager.dart';
 import 'package:wedplan/views/checklist/create_checklist.dart';
@@ -14,88 +15,98 @@ class ChecklistScreen extends StatefulWidget {
 class _ChecklistScreenState extends State<ChecklistScreen> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Consumer<DbManagerChecklist>(
-        builder: (context, manager, child) {
-          final itemCheck = manager.checklistModels;
-          return ListView.builder(
-            itemCount: itemCheck.length,
-            itemBuilder: (BuildContext context, int index) {
-              final item = itemCheck[index];
-              return InkWell(
-                onTap: () async {
-                  final selectedTask = await manager.getChecklistById(item.id!);
-                  // ignore: use_build_context_synchronously
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CreateChecklist(
-                        checklistModels: selectedTask,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF17203A),
+        title: Text(
+          'Daftar Plan',
+          style: GoogleFonts.acme(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<DbManagerChecklist>(
+          builder: (context, manager, child) {
+            final itemCheck = manager.checklistModels;
+            return ListView.builder(
+              itemCount: itemCheck.length,
+              itemBuilder: (BuildContext context, int index) {
+                final item = itemCheck[index];
+                return InkWell(
+                  onTap: () async {
+                    final selectedTask =
+                        await manager.getChecklistById(item.id!);
+                    // ignore: use_build_context_synchronously
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CreateChecklist(
+                          checklistModels: selectedTask,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            itemCheck[index].task!,
+                            style: TextStyle(
+                              overflow: TextOverflow.ellipsis,
+                              color: itemCheck[index].isCompleted
+                                  ? Colors.grey
+                                  : null,
+                              decoration: itemCheck[index].isCompleted
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  content: const Text(
+                                    'Are you sure?',
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text(
+                                        'NO',
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          manager.deleteChecklist(item.id!);
+                                        });
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text(
+                                        'YES',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            icon: const Icon(Icons.delete),
+                          )
+                        ],
                       ),
                     ),
-                  );
-                },
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          itemCheck[index].task!,
-                          style: TextStyle(
-                            overflow: TextOverflow.ellipsis,
-                            color: itemCheck[index].isCompleted
-                                ? Colors.grey
-                                : null,
-                            decoration: itemCheck[index].isCompleted
-                                ? TextDecoration.lineThrough
-                                : null,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                content: const Text(
-                                  'Are you sure?',
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text(
-                                      'NO',
-                                    ),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        manager.deleteChecklist(item.id!);
-                                      });
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text(
-                                      'YES',
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                          icon: const Icon(Icons.delete),
-                        )
-                      ],
-                    ),
                   ),
-                ),
-              );
-            },
-          );
-        },
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
